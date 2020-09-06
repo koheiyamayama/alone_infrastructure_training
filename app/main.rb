65c4e812bc21f19657635d00d12e0b41dcacfdda
koheiyamayama <kohei.ccw.23@gmail.com>
2020-09-06 17:22:50 +0900

    mariadbに繋がるまでconnectをここみるようにした

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,32 @@
+require 'mysql2'
+
+client=''
+success_connect_to_mysql = false
+until success_connect_to_mysql
+  puts "\e[34m connecting to mariadb... \e[0m"
+  sleep 5
+  begin
+    client = Mysql2::Client.new(
+      host: ENV['DATABASE_HOSTNAME'],
+      password: ENV['DATABASE_PASSWORD'],
+      port: ENV['DATABASE_PORT'],
+      username: ENV['DATABASE_USERNAME'],
+      reconnect: true
+    )
+  rescue Mysql2::Error::ConnectionError => exception
+    next
+  rescue StandardError => exception
+    raise exception
+    p $!
+    p $@
+  end
+  success_connect_to_mysql = true
+  puts "\e[32m success!! connect to mariadb!! \e[0m"
+end
+
+p client
+
+
 puts 'Hello World!'
 count = 0
 while true
