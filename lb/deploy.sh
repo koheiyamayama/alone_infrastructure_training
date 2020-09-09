668601388342ad198459fd1d8eba0d4baf894da6
koheiyamayama <kohei.ccw.23@gmail.com>
2020-09-09 23:13:40 +0900

    nginxのconfファイルをLBに配置して、restartするスクリプトを書いた

---
+++ lb/deploy.sh
@@ -0,0 +1,11 @@
+echo "updating nginx configuration..."
+scp ./lb/conf/nginx.conf pi@192.168.11.7:/etc/nginx/nginx.conf &&
+scp ./lb/conf/main.conf pi@192.168.11.7:/etc/nginx/conf.d/main.conf
+if [ $? = 0 ]; then
+  printf "\e[32m completing the updating!! \e[m"
+else
+  printf "\e[31m update failed!! \e[m\n"
+fi
+echo -e "\n"
+echo "restarting nginx process..."
+ssh pi@192.168.11.7 "sudo systemctl restart nginx.service"
