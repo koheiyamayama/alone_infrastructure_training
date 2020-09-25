2b7a9b9019cad0b086cea79918db8ac2e90ed034
koheiyamayama <kohei.ccw.23@gmail.com>
2020-09-25 21:26:39 +0900

    prettier書けた

---
+++ frontend/src/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Kannzenshouri from "./images/kannzennshouri_uzakichan.jpg"
+ReactDOM.render(<div>hello world!<img src={Kannzenshouri} /></div>, document.getElementById("root"));
---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: process.env.NODE_ENV, entry: "./src/index.tsx", output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "build.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/, use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      }
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })]
+};
