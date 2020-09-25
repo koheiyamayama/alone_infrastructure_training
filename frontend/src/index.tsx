240e24af226caf1b23b99ead71f390e202d3e1f8
koheiyamayama <kohei.ccw.23@gmail.com>
2020-09-25 23:16:10 +0900

    storybook導入した

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Kannzenshouri from "./images/kannzennshouri_uzakichan.jpg";
-ReactDOM.render(
-  <div>
+
+export const App: React.FC = () => {
+  return <div>
     hello world!
     <img src={Kannzenshouri} />
-  </div>,
+  </div>
+}
+
+ReactDOM.render(
+  <App />,
   document.getElementById("root")
 );
---
+++ frontend/src/stories/App.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { App } from "../index";
+export default {
+  title: "App",
+  component: App,
+};
+export const Component = () => <App></App>;
+Component.story = {
+  name: "Component",
+};
