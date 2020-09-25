2eb0fd62874b9b496f6427ec35cdad51be460c51
koheiyamayama <kohei.ccw.23@gmail.com>
2020-09-25 21:58:10 +0900

    jest入れた

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,28 +1,35 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  mode: process.env.NODE_ENV, entry: "./src/index.tsx", output: {
+  mode: process.env.NODE_ENV,
+  entry: "./src/index.tsx",
+  output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "build.js",
   },
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/, use: [
+        test: /\.(ts|tsx)$/,
+        use: [
           {
             loader: "ts-loader",
           },
         ],
       },
       {
-        test: /\.(png|jpg)$/,
-        use: [
-          {
-            loader: "file-loader"
-          }
-        ]
-      }
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png)$/,
+        use: ["file-loader"],
+      },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })]
+  resolve: {
+    extensions: [".ts", ".js", ".tsx"],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
 };
