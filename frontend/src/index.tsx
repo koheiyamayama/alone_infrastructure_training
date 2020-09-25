6ced7a2e920d4abda42799f89b1bc09411693ce4
koheiyamayama <kohei.ccw.23@gmail.com>
2020-09-25 21:30:02 +0900

    prettier掛けた

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Kannzenshouri from "./images/kannzennshouri_uzakichan.jpg"
-ReactDOM.render(<div>hello world!<img src={Kannzenshouri} /></div>, document.getElementById("root"));
+import Kannzenshouri from "./images/kannzennshouri_uzakichan.jpg";
+ReactDOM.render(
+  <div>
+    hello world!
+    <img src={Kannzenshouri} />
+  </div>,
+  document.getElementById("root")
+);
