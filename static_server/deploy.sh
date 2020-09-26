96a463a97e38c7e7d16931b01fd866f0db1fa62e
koheiyamayama <kohei.ccw.23@gmail.com>
2020-09-27 00:36:05 +0900

    本番でbuild済みのjsを表示できるようにした

---
+++ static_server/deploy.sh
@@ -0,0 +1,18 @@
+set -e
+ssh static_server "sudo mkdir -p -m 777 /assets"
+scp -r $PWD/frontend/dist/* static_server:/assets/
+scp -r $PWD/static_server/conf/nginx.conf static_server:/etc/nginx/nginx.conf
+if [ $? = 0 ]; then
+  echo "success for distributing files"
+else
+  echo "failed for distributing files"
+  exit 1
+fi
+ssh static_server "sudo systemctl daemon-reload && sudo systemctl restart nginx.service"
+if [ $? = 0 ]; then
+  echo "success for restarting nginx process"
+  exit 0
+else
+  echo "failed for restarting nginx process"
+  exit 1
+fi
