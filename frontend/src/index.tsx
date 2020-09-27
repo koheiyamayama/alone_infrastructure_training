073fda533d50c8f87a722764ea6207102528c406
koheiyamayama <kohei.ccw.23@gmail.com>
2020-09-27 22:50:32 +0900

    niyaniya_uzakichanを追加した

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import Kannzenshouri from "./images/kannzennshouri_uzakichan.jpg";
 import Megane from "./images/megane_uzakichan.jpg"
 import Ukkari from "./images/ukkari_uzakichan.jpg"
+import Niyaniya from "./images/niyaniya_uzakichan.jpg"
+
 
 export const App: React.FC = () => {
   return <div>
@@ -10,6 +12,7 @@ export const App: React.FC = () => {
     <img src={Kannzenshouri} />
     <img src={Megane} />
     <img src={Ukkari} />
+    <img src={Niyaniya} />
   </div>
 }
 
