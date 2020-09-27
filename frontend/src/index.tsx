c701f37b03bd113a2dc005bec2c5eb0ef7d702f9
koheiyamayama <kohei.ccw.23@gmail.com>
2020-09-27 13:12:17 +0900

    webpack-dev-serverの設定を正しくすれば問題なさそうなところまで来た

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Kannzenshouri from "./images/kannzennshouri_uzakichan.jpg";
+import Megane from "./images/megane_uzakichan.jpg"
 
 export const App: React.FC = () => {
   return <div>
     hello world!
     <img src={Kannzenshouri} />
+    <img src={Megane} />
   </div>
 }
 
